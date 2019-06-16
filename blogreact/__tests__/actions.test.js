a8865ec66efd52cad8b0bf6ba107738e311eb528
Olivia Wang <oliviawang@OliviadeMacBook-Pro.local>
2019-06-16 22:58:09 +0800

    not test

--- blogreact/__tests__/actions.test.js
+++ blogreact/__tests__/actions.test.js
@@ -1,94 +1,94 @@
-import getContent from '../src/actions/contents';
+// import getContent from '../src/actions/contents';
 
-/* const getContent = require('../src/actions/contents');*/
+// /* const getContent = require('../src/actions/contents');*/
 
-/* 测试 bizTable 相关 actions */
-describe('test actions', () => {
+// /* 测试 bizTable 相关 actions */
+// describe('test actions', () => {
 
-    // /* 测试get data */
-    // test('should create an action for get data request', (contents) => {
-    //     const expectedAction = {
-    //         type: 'GET_CONTENT',
-    //         contents
-    //     };
+//     // /* 测试get data */
+//     // test('should create an action for get data request', (contents) => {
+//     //     const expectedAction = {
+//     //         type: 'GET_CONTENT',
+//     //         contents
+//     //     };
 
-    //     expect(getContent).toEqual(expectedAction);
-    // });
+//     //     expect(getContent).toEqual(expectedAction);
+//     // });
 
-    // test('get data 异步', () => {
-    //     expect.assertions(1);
-    //     return getContent
-    //         .then(content => {
-    //             expect(content.id).toBe(3);
-    //         });
-    // });
+//     // test('get data 异步', () => {
+//     //     expect.assertions(1);
+//     //     return getContent
+//     //         .then(content => {
+//     //             expect(content.id).toBe(3);
+//     //         });
+//     // });
 
-    test('getContent能被调用', async () => {
-        beforeEach(() => {
-            getContent.mockClear();
-        });
-        
-        expect.assertions(1);
-        let mockFn = jest.fn();
-        await getContent(mockFn);
+//     test('getContent能被调用', async () => {
+//         beforeEach(() => {
+//             getContent.mockClear();
+//         });
 
-        // 断言mockFn被调用
-        expect(mockFn).toBeCalled();
-    });
+//         expect.assertions(1);
+//         let mockFn = jest.fn();
+//         await getContent(mockFn);
 
-    // /* 测试put data */
-    // test('should create an action for successful result of get data', () => {
-    //     const payload = {
-    //         items: [{
-    //                 id: 1,
-    //                 code: '1'
-    //             },
-    //             {
-    //                 id: 2,
-    //                 code: '2'
-    //             }
-    //         ],
-    //         total: 2
-    //     };
-    //     const expectedAction = {
-    //         type: type.BIZ_TABLE_GET_RES_SUCCESS,
-    //         payload
-    //     };
+//         // 断言mockFn被调用
+//         expect(mockFn).toBeCalled();
+//     });
 
-    //     expect(actions.addContent(payload)).toEqual(expectedAction);
-    // });
+//     // /* 测试put data */
+//     // test('should create an action for successful result of get data', () => {
+//     //     const payload = {
+//     //         items: [{
+//     //                 id: 1,
+//     //                 code: '1'
+//     //             },
+//     //             {
+//     //                 id: 2,
+//     //                 code: '2'
+//     //             }
+//     //         ],
+//     //         total: 2
+//     //     };
+//     //     const expectedAction = {
+//     //         type: type.BIZ_TABLE_GET_RES_SUCCESS,
+//     //         payload
+//     //     };
 
-    // /* 测试edit table */
-    // test('should create an action for update table params', () => {
-    //     const payload = {
-    //         paging: {
-    //             current: 2,
-    //             pageSize: 25
-    //         }
-    //     };
-    //     const expectedAction = {
-    //         type: type.BIZ_TABLE_PARAMS_UPDATE,
-    //         payload
-    //     };
+//     //     expect(actions.addContent(payload)).toEqual(expectedAction);
+//     // });
 
-    //     expect(actions.editContent(payload)).toEqual(expectedAction);
-    // });
+//     // /* 测试edit table */
+//     // test('should create an action for update table params', () => {
+//     //     const payload = {
+//     //         paging: {
+//     //             current: 2,
+//     //             pageSize: 25
+//     //         }
+//     //     };
+//     //     const expectedAction = {
+//     //         type: type.BIZ_TABLE_PARAMS_UPDATE,
+//     //         payload
+//     //     };
 
-    // /* 测试remove table */
-    // test('should create an action for update table params', () => {
-    //     const payload = {
-    //         paging: {
-    //             current: 2,
-    //             pageSize: 25
-    //         }
-    //     };
-    //     const expectedAction = {
-    //         type: type.BIZ_TABLE_PARAMS_UPDATE,
-    //         payload
-    //     };
+//     //     expect(actions.editContent(payload)).toEqual(expectedAction);
+//     // });
 
-    //     expect(actions.editContent(payload)).toEqual(expectedAction);
-    // });
+//     // /* 测试remove table */
+//     // test('should create an action for update table params', () => {
+//     //     const payload = {
+//     //         paging: {
+//     //             current: 2,
+//     //             pageSize: 25
+//     //         }
+//     //     };
+//     //     const expectedAction = {
+//     //         type: type.BIZ_TABLE_PARAMS_UPDATE,
+//     //         payload
+//     //     };
 
-});
+//     //     expect(actions.editContent(payload)).toEqual(expectedAction);
+//     // });
+
+// });
 
