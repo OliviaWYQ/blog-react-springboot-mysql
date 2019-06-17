f2f46c9945d1e56fa1150043af297aea688745e9
Olivia Wang <oliviawang@OliviadeMacBook-Pro.local>
2019-06-17 10:07:36 +0800

    img

---
+++ blogreact/mocks/actionsmock.js
@@ -0,0 +1,12 @@
+// export const mock = jest.fn();
+// const mock = jest.fn();
+
+// const mock = jest.fn().mockImplementation(() => {
+//     const data = {
+        
+//     };
+
+//     return data;
+// });
+
+// export default mock;
